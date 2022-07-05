6b628773456d6cb60c3deebbcdda5b846bebafb0
Kyle Drotsky <kyledrotsky@Kyles-MacBook-Air.local>
2022-07-05 15:07:44 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,120 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import "antd/dist/antd.css";
+import "./index.css";
+import { Layout, Menu } from "antd";
+import {
+  HomeOutlined,
+  FireOutlined,
+  QuestionOutlined,
+  CameraOutlined,
+  CodepenOutlined,
+  ExperimentOutlined,
+  PictureOutlined,
+  VideoCameraOutlined,
+  SkinOutlined,
+  LaptopOutlined,
+} from "@ant-design/icons";
+import { Wallpapers } from "./components/Wallpapers";
+import { Renders } from "./components/Renders";
+import { Experimental } from "./components/Experimental";
+import { CurrentEvents } from "./components/CurrentEvents";
+import { Nature } from "./components/Nature";
+import { Business } from "./components/Business";
+import { Film } from "./components/Film";
+import { Fashion } from "./components/Fashion";
+import { Architecture } from "./components/Architecture";
+import { Textures } from "./components/Textures";
 
-function App() {
+// component to render all the other functions
+const { Header, Content, Footer, Sider } = Layout;
+
+const App = () => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const onCollapse = () => {
+    setCollapsed(!collapsed);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sider collapsible collapsed={!collapsed} onCollapse={onCollapse}>
+          <div className="logo" />
+          <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+            <Menu.Item key="1" icon={<QuestionOutlined />}>
+              Current Events
+              <Link to="/" />
+            </Menu.Item>
+            <Menu.Item key="2" icon={<PictureOutlined />}>
+              Wallpapers
+              <Link to="/wallpapers" />
+            </Menu.Item>
+            <Menu.Item key="3" icon={<CodepenOutlined />}>
+              3D Renders
+              <Link to="/3d" />
+            </Menu.Item>
+            <Menu.Item key="4" icon={<FireOutlined />}>
+              Textures & Patterns
+              <Link to="/textures" />
+            </Menu.Item>
+            <Menu.Item key="5" icon={<ExperimentOutlined />}>
+              Experimental
+              <Link to="/experimental" />
+            </Menu.Item>
+            <Menu.Item key="6" icon={<HomeOutlined />}>
+              Architecture
+              <Link to="/architecture" />
+            </Menu.Item>
+            <Menu.Item key="7" icon={<CameraOutlined />}>
+              Nature
+              <Link to="/nature" />
+            </Menu.Item>
+            <Menu.Item key="8" icon={<LaptopOutlined />}>
+              Business & Work
+              <Link to="/business" />
+            </Menu.Item>
+            <Menu.Item key="9" icon={<SkinOutlined />}>
+              Fashion
+              <Link to="/fashion" />
+            </Menu.Item>
+            <Menu.Item key="10" icon={<VideoCameraOutlined />}>
+              Film
+              <Link to="/film" />
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header style={{ textAlign: "center", color: "white", fontSize: 32 }}>
+            Resplash
+          </Header>
+          <Content
+            style={{
+              margin: "24px 16px",
+              padding: 24,
+              background: "#fff",
+              minHeight: 280,
+            }}
+          >
+            <Routes>
+              <Route path="/" element={<CurrentEvents />} />
+              <Route path="/wallpapers" element={<Wallpapers />} />
+              <Route path="/3d" element={<Renders />} />
+              <Route path="/textures" element={<Textures />} />
+              <Route path="/experimental" element={<Experimental />} />
+              <Route path="/architecture" element={<Architecture />} />
+              <Route path="/nature" element={<Nature />} />
+              <Route path="/business" element={<Business />} />
+              <Route path="/fashion" element={<Fashion />} />
+              <Route path="/film" element={<Film />} />
+            </Routes>
+          </Content>
+          <Footer style={{ textAlign: "center" }}>Kyle Drotsky 2022</Footer>
+        </Layout>
+      </Layout>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/components/CurrentEvents.tsx
@@ -0,0 +1,74 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./components.css";
+import "antd/dist/antd.css";
+import { CaretLeftOutlined, CaretRightOutlined } from "@ant-design/icons";
+
+export const CurrentEvents = () => {
+  const [image, setImage] = useState<any>();
+  const [results, setResults] = useState<any[]>([]);
+  const url =
+    "https://api.unsplash.com/collections?current-events" +
+    image +
+    "&client_id=" +
+    process.env.clientid;
+
+  const getCurrentEvents = async () => {
+    axios.get(url).then((response) => {
+      console.log(response);
+      setResults(response.data.results);
+    });
+  };
+
+  useEffect(() => {
+    getCurrentEvents();
+  });
+
+  const [scrollX, setScrollX] = useState(0);
+
+  const handleLeftArrow = () => {
+    let x = scrollX + Math.round(window.innerWidth / 4);
+    if (x > 0) {
+      x = 0;
+    }
+    setScrollX(x);
+  };
+
+  const handleRightArrow = () => {
+    let x = scrollX - Math.round(window.innerWidth / 4);
+    let listW = results.length * 150;
+    if (window.innerWidth - listW > x) {
+      x = window.innerWidth - listW - 60;
+    }
+    setScrollX(x);
+  };
+
+  return (
+    <div className="imageRow">
+      <h2>Current Events</h2>
+      <div className="imageRow--left" onClick={handleLeftArrow}>
+        <CaretLeftOutlined style={{ fontSize: 50 }} />
+      </div>
+
+      <div className="imageRow--right" onClick={handleRightArrow}>
+        <CaretRightOutlined style={{ fontSize: 50 }} />
+      </div>
+      <div className="imageRow--listarea">
+        <div
+          className="imageRow--list"
+          style={{
+            marginLeft: scrollX,
+            width: results.length * 150,
+          }}
+        >
+          {results.length > 0 &&
+            results.map((image) => (
+              <div className="imageRow--item">
+                <img src={image.urls.small} key={image.id} alt={image.title} />
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
